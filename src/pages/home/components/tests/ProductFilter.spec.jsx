374fbabf14d89f8c586e0ec8e933aecef51b12d9
sonsurim <surim014@naver.com>
2024-02-12 23:11:25 +0900

    Section5. 통합테스트 작성하기

--- src/pages/home/components/tests/ProductFilter.spec.jsx
+++ src/pages/home/components/tests/ProductFilter.spec.jsx
@@ -10,12 +10,48 @@ const setMinPriceFn = vi.fn();
 const setMaxPriceFn = vi.fn();
 const setTitleFn = vi.fn();
 
-beforeEach(() => {});
+beforeEach(() => {
+  mockUseFilterStore({
+    setMinPrice: setMinPriceFn,
+    setMaxPrice: setMaxPriceFn,
+    setTitle: setTitleFn,
+  });
+});
 
-it('카테고리 목록을 가져온 후 카테고리 필드의 정보들이 올바르게 렌더링된다.', async () => {});
+it('카테고리 목록을 가져온 후 카테고리 필드의 정보들이 올바르게 렌더링된다.', async () => {
+  await render(<ProductFilter />);
 
-it('상품명을 수정하는 경우 setTitle 액션이 호출된다.', async () => {});
+  expect(await screen.findByLabelText('category1')).toBeInTheDocument();
+  expect(await screen.findByLabelText('category2')).toBeInTheDocument();
+  expect(await screen.findByLabelText('category3')).toBeInTheDocument();
+});
 
-it('카테고리를 클릭 할 경우의 클릭한 카테고리가 체크된다.', async () => {});
+it('상품명을 수정하는 경우 setTitle 액션이 호출된다.', async () => {
+  const { user } = await render(<ProductFilter />);
 
-it('최소 가격 또는 최대 가격을 수정하면 setMinPrice과 setMaxPrice 액션이 호출된다.', async () => {});
+  const TextInput = screen.getByLabelText('상품명');
+  await user.type(TextInput, 'test');
+
+  expect(setTitleFn).toBeCalledWith('test');
+});
+
+it('카테고리를 클릭 할 경우의 클릭한 카테고리가 체크된다.', async () => {
+  const { user } = await render(<ProductFilter />);
+
+  const Category3 = await screen.findByLabelText('category3');
+  await user.click(Category3);
+
+  expect(Category3).toBeChecked();
+});
+
+it('최소 가격 또는 최대 가격을 수정하면 setMinPrice과 setMaxPrice 액션이 호출된다.', async () => {
+  const { user } = await render(<ProductFilter />);
+  const MinPriceTextInput = screen.getByPlaceholderText('최소 금액');
+  const MaxPriceTextInput = screen.getByPlaceholderText('최대 금액');
+
+  await user.type(MinPriceTextInput, '1');
+  await user.type(MaxPriceTextInput, '2');
+
+  expect(setMinPriceFn).toBeCalledWith('1');
+  expect(setMaxPriceFn).toBeCalledWith('2');
+});
